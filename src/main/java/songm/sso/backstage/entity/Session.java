7848ff5b49facd1d8c9aaaf9037bf61cfcc3ce71
songzigw <songzigw@163.com>
2016-11-21 15:42:52 +0800

    sso

--- src/main/java/songm/sso/backstage/entity/Session.java
+++ src/main/java/songm/sso/backstage/entity/Session.java
@@ -24,8 +24,8 @@ import java.util.Map;
 /**
  * 用户与服务端的会话
  *
- * @author  zhangsong
- * @since   0.1, 2016-7-29
+ * @author zhangsong
+ * @since 0.1, 2016-7-29
  * @version 0.1
  * 
  */
@@ -35,64 +35,64 @@ public class Session implements Serializable {
 
     /** 保存在客户端session标识 */
     public static final String USER_SESSION_KEY = "songm_session";
-    
+
     public static final String USER_INFO = "user_info";
 
     /** 会话唯一标示 */
-	private String sesId;
+    private String sesId;
 
-	/** 用户ID */
+    /** 用户ID */
     private String userId;
 
-	/** 会话创建时间 */
-	private Date createdTime;
+    /** 会话创建时间 */
+    private Date created;
 
-	/** 会话访问时间 */
-	private Date accessTime;
+    /** 会话访问时间 */
+    private Date access;
 
-	private Map<String, Object> attribute;
-	
-	public Session(String sessionId) {
-		this.sesId = sessionId;
-		createdTime = new Date();
-		accessTime = createdTime;
-	}
+    private Map<String, Object> attribute;
 
-	public String getSesId() {
-		return sesId;
-	}
+    public Session(String sessionId) {
+        this.sesId = sessionId;
+        created = new Date();
+        access = created;
+    }
 
-	public void setSesId(String id) {
-		this.sesId = id;
-	}
+    public String getSesId() {
+        return sesId;
+    }
 
-	public Object getAttribute(String name) {
-		if (attribute == null) {
-			return null;
-		}
-		return attribute.get(name);
-	}
+    public void setSesId(String id) {
+        this.sesId = id;
+    }
 
-	public void setAttribute(String name, Object value) {
-		if (attribute == null) {
-			attribute = new HashMap<String, Object>();
-		}
-		attribute.put(name, value);
-	}
+    public Object getAttribute(String name) {
+        if (attribute == null) {
+            return null;
+        }
+        return attribute.get(name);
+    }
 
-	public void setCreatedTime(Date createdTime) {
-		this.createdTime = createdTime;
-	}
+    public void setAttribute(String name, Object value) {
+        if (attribute == null) {
+            attribute = new HashMap<String, Object>();
+        }
+        attribute.put(name, value);
+    }
 
-	public void setAccessTime(Date accessTime) {
-		this.accessTime = accessTime;
-	}
+    public void setCreated(Date created) {
+        this.created = created;
+    }
 
-	public long getCreatedTime() {
-		return createdTime.getTime();
-	}
+    public void setAccess(Date access) {
+        this.access = access;
+    }
 
-	public String getUserId() {
+    public long getCreated() {
+        return created.getTime();
+    }
+
+    public String getUserId() {
         return userId;
     }
 
@@ -100,15 +100,15 @@ public class Session implements Serializable {
         this.userId = userId;
     }
 
-    public long getAccessTime() {
-		return accessTime.getTime();
-	}
+    public long getAccess() {
+        return access.getTime();
+    }
 
-	public void updateAccessTime() {
-		accessTime = new Date();
-	}
+    public void updateAccess() {
+        access = new Date();
+    }
 
-	@Override
+    @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
@@ -118,18 +118,13 @@ public class Session implements Serializable {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
         Session other = (Session) obj;
         if (sesId == null) {
-            if (other.sesId != null)
-                return false;
-        } else if (!sesId.equals(other.sesId))
-            return false;
+            if (other.sesId != null) return false;
+        } else if (!sesId.equals(other.sesId)) return false;
         return true;
     }
 }
